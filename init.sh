ea0ec06fa3aee6b8092258770e3a65ecfe702c35
Nicolas Zilli <m.nicolas.zilli@gmail.com>
2020-05-30 18:57:47 +1000

    Add dependency and dependency management

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
 export GEOMETRY=$PWD
 export PYTHONPATH=$GEOMETRY/..:$PYTHONPATH
 export PATH=$GEOMETRY:$PATH
+
+# Install all dependencies listed in the requirements.txt file
+pip install -r requirements.txt
