52d2fa1246cbf87cb903943536f0fe1e2a20da78
GaelTouquet <gael.touquet@gmail.com>
2020-06-12 08:58:34 +0200

    attempt to make full matlab

--- test_matlab.py
+++ test_matlab.py
@@ -2,6 +2,6 @@ from Objects.Patterns import SpiralPhyllotaxisPattern
 
 fibo = 1
 phyllo = SpiralPhyllotaxisPattern(
-    8*2584, 2584, n_readouts=100, time_per_acquisition=0.006, alternated_points=True)
+    8*2584, 2584, n_readouts=272, alternated_points=True)
 
-# phyllo.write_to_matlab('./matlab_test.mat','k')
+phyllo.write_to_matlab('./matlab_test.mat','myk')
